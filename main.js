33c752942639ef98faca1bd2f2a070cb271a1139
EdelweissPirate <joshmarkevans@gmail.com>
2023-03-14 09:55:57 -0500

    set up readable stream

--- main.js
+++ main.js
@@ -9,26 +9,53 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static(path.join(__dirname, "/public/ARP-L_LESSON_2_IMPLEMENTATION_PART_1")));
 
 const getVideo = (req, res) => {
+    let range = req.headers.range;
+
+    if(!range || range === undefined) {
+        range = 'bytes=0-'; //res.status(400).send("Requires Range header");
+    }
+
+    // config
     let params = (new URL(req.headers.referer + req.url)).searchParams;
 
     const urlParams = new URLSearchParams(params);
     const videoName = urlParams.get('videoName')
 
-    const filename = __dirname + `/public/${videoName}/output.mpd`;
+    const filename = __dirname + `/public/testVid.mp4` //${videoName}/output.mpd`;
+    // end config
 
-    const readStream = fs.createReadStream(filename);
+    // 3rd attempt
+    const videoSize = fs.statSync(filename).size;
+    const CHUNK_SIZE = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, headers);
+    const videoStream = fs.createReadStream(filename, { start, end });
+    videoStream.pipe(res);
+    //
 
-    readStream.on('open', function () {
-        // This just pipes the read stream to the response object (which goes to the client)
-        readStream.pipe(res); // <====== here
-    });
+    // 2nd attempt
+    // const readStream = fs.createReadStream(filename);
 
-    // This catches any errors that happen while creating the readable stream (usually invalid names)
-    readStream.on('error', function(err) {
-        res.end(err);
-    });
+    // readStream.on('open', function () {
+    //     // This just pipes the read stream to the response object (which goes to the client)
+    //     readStream.pipe(res); // <====== here
+    // });
 
-    // res.sendFile(__dirname + `/public/${videoName}/output.mpd`);
+    // // This catches any errors that happen while creating the readable stream (usually invalid names)
+    // readStream.on('error', function(err) {
+    //     res.end(err);
+    // });
+
+    // OG
+    // res.sendFile(filename);
 }
 
 app.get("/getVideo", protect, getVideo);
